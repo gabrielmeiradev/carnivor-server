595449f158e6ac666654e5305d7bb875a8ca6b95
gabrielmeiradev <gabriel_meira10@hotmail.com>
2025-04-23 15:09:05 -0300

    fix: mandatory images

--- src/controllers/post/create.ts
+++ src/controllers/post/create.ts
@@ -25,6 +25,11 @@ export const createPost = async (req: Request, res: Response) => {
 
   const images = req.files as Express.Multer.File[];
 
+  if (images.length <= 0) {
+    res.status(400).json({ message: "Nenhuma imagem enviada" });
+    return;
+  }
+
   if (parent_id) {
     try {
       const parentPost = await prisma.post.update({
